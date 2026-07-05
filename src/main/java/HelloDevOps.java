69f653df1aabde93920ec9a3c944c895d09967db
DevNixOps <vikram.lsj@gmail.com>
2026-07-05 20:48:09 +0530

    move to src dir

---
+++ src/main/java/HelloDevOps.java
@@ -0,0 +1,11 @@
+public class HelloDevOps {
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println(" Welcome to DevOps Crash Course ");
+        System.out.println(" Hello DevOps!");
+        System.out.println("===================================");
+
+    }
+}
